c95a883ce34bd6fce4265c07104f3a238df15a98
cheroliv <cheroliv@laposte.net>
2019-09-08 23:47:53 +0200

    ___

---
+++ chap-02-03-start-reservations/src/main/groovy/com/cheroliv/ReservationsApplication.groovy
@@ -0,0 +1,12 @@
+package com.cheroliv;
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ReservationsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ReservationsApplication.class, args)
+	}
+}
---
+++ chap-02-03-start-reservations/src/test/groovy/com/cheroliv/ReservationsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cheroliv;
+
+import groovy.transform.CompileStatic;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CompileStatic
+@SpringBootTest
+@ExtendWith(SpringExtension)
+public class ReservationsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
